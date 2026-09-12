3190c1b095367ccca3762b446de3f99807d1c301
Aleksey341 <108056836+Aleksey341@users.noreply.github.com>
2026-09-12 22:31:50 +0300

    feat: enable catch-up request boundaries in module GUI

--- meeting_bridge/__main__.py
+++ meeting_bridge/__main__.py
@@ -32,11 +32,13 @@ def main() -> None:
 
         mcp_main()
     elif args.cmd == "gui":
+        from meeting_bridge.catchup_boundary_runtime import install as install_catchup_boundary
         from meeting_bridge.professional_preflight import install as install_professional_preflight
         from meeting_bridge.quality_runtime import install as install_quality_runtime
 
         install_quality_runtime()
         install_professional_preflight()
+        install_catchup_boundary()
         from meeting_bridge.gui_runtime import main as gui_main
 
         gui_main()
